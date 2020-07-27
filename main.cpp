bb201563672cbddf27fd984f1ef3a4d581738ddc
Hakan Halil <hhalil@vmware.com>
2020-07-27 22:54:39 +0300

    Add troubling concepts and immitate simple free list init

--- main.cpp
+++ main.cpp
@@ -7,12 +7,18 @@ public:
 };
 
 class LinkedList {
-    Node* head;
 
 public:
+    Node* head;
+
+    LinkedList() {
+        head = nullptr;
+    }
+
     void Push(Node* newNode) {
         if (head == nullptr) {
             head = newNode;
+            return;
         }
 
         Node* current = this->head;
@@ -54,9 +60,45 @@ class Allocator {
     size_t free_list_level_limit;
 
     size_t overheadSize;
+    size_t overhead_blocks_count;
 
 public:
-    // TODO: How to order inner structures' data after they are initialized
+    void addNMinimumSizedBlocks(size_t count) {
+        // if we have filled uneven amount of leaf nodes, the last leaf node must join the free list
+        if (count % 2 == 1) {
+            size_t offset = count * min_block_size;
+
+
+            // TODO: This must be done for every level, what happens when this block is allocated? Currently 1 block (16byte) is used for the LL and the NewNode
+            // Suppose we have BL 1 -> BL 2 -> BL 3
+            // BL 1 has the LinkedList that is pointed to by freeLists[i] and the head Node
+            // BL 2 is pointed to by the head node and has its next node
+            // BL 3 is pointed to by the BL 2 next node and its next is NULL
+            // What happens if we need to give BL1 for allocation?
+            // We have to remove the head (no problem)
+            // BUT WE ALSO HAVE TO REMOVE THE LL ITSELF, WE NEED TO MOVE IT NEXT TO BL2
+            // or in other words push BL2 a couple of bytes to the right so as to make space for the linked list
+            // See if we can actually store the actual linked list in the last BL?
+            // Does this mean that Push operation must always move the linked list to the new memory block?
+            // CAN THIS BE RESOLVED BY REMOVING THE LINKED LIST ABSTRACTION AND USING THE NODE* ONLY?
+
+            // LL is not init, here we are initing it
+            LinkedList* ll =((LinkedList *)((char*)base_ptr + offset));
+            ll->head = nullptr;
+
+            offset += sizeof(LinkedList);
+            Node *newNode = (Node*)((char *)base_ptr + offset);
+            newNode->next = nullptr;
+            ll->Push(newNode);
+
+            freeLists[free_list_count - 1] = ll;
+            if (true) {
+                int a = 5 + 4;
+            }
+        }
+    }
+
+// TODO: How to order inner structures' data after they are initialized
     // TODO: Preamble vs Other ways for size persistence
     Allocator(void* addr, size_t size) {
         base_ptr = (uint8_t *)(addr);
@@ -72,6 +114,7 @@ public:
         freeLists = (LinkedList**) addr;
         for (int i = 0; i < free_list_count; ++i) {
             freeLists[i] = ((LinkedList*) addr) + i;
+            freeLists[i] = nullptr;
         }
 
         // HERE: addr + 3 -> NEXT: addr + 4 ( 4 x 8 )
@@ -87,6 +130,10 @@ public:
 
         // must find X = overheadSize % min_block_size and mark the first X nodes as used
         // Update isSplit and freeLists with new values
+        this->overhead_blocks_count = ceil(overheadSize / float(min_block_size));
+
+        // updates all necessary inner structures with current state
+        addNMinimumSizedBlocks(this->overhead_blocks_count);
     }
 
     void* Allocate(size_t size) {
