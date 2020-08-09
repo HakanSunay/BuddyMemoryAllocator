1d890190562b30d84594b8d1a6ad947e42e2598c
Hakan Halil <hhalil@vmware.com>
2020-08-09 17:19:47 +0300

    Add missing semicolon

--- Exception.h
+++ Exception.h
@@ -8,7 +8,7 @@
 #include <stdexcept>
 
 const char* BUDDY_INIT_EXCEPTION_MSG = "Allocator cannot be initialized with size less than twice of minimum block size";
-const char* BUDDY_INIT_WITH_NULLPTR_EXCEPTION_MSG = "Allocator cannot be initialized using nullptr as the memory block to be managed"
+const char* BUDDY_INIT_WITH_NULLPTR_EXCEPTION_MSG = "Allocator cannot be initialized using nullptr as the memory block to be managed";
 const char* BUDDY_FREE_EXCEPTION_MSG = "Input address is not managed by the Allocator";
 
 class Exception: public std::runtime_error {
